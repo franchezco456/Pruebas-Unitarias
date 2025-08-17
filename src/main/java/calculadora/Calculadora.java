77e9f9e92a6c461ad8d7636a8f94f07aa80bd4e5
franky franco <frankyfranco456@gmail.com>
2025-08-17 14:40:39 -0500

    se agregan las excepciones para la suma y la division

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -3,14 +3,22 @@ package calculadora;
 public class Calculadora {
 
     public int sumar(int a, int b) {
-        return a + b;
+        try {
+            return a + b;
+        }catch(ArithmeticException e) {
+            throw new ArithmeticException("Error al ejecutar la suma");
+        }
+
     }
 
     public int dividir(int a, int b) {
         if (b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
-        return a / b;
+        try{
+            return a / b;
+        }catch(ArithmeticException e) {
+            throw new ArithmeticException("Error al ejecutar la division");
+        }
     }
-
 
     public int restar(int a, int b) {
         return a - b;
