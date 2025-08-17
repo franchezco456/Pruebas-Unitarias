c27e8c07b321937b237870df58967cad8be0ebc7
Rafael <augustico10@hotmail.com>
2025-08-17 16:02:47 -0500

    Prubas unitarias de resta y multiplicacion de la clase calculadora.

---
+++ src/test/calculadora/CalculadoraTestRafa.java
@@ -0,0 +1,52 @@
+package calculadora;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTestRafa {
+
+    private final Calculadora calc = new Calculadora();
+
+
+    @Test
+    void restarNormal() {
+        assertEquals(10, calc.restar(15, 5));
+    }
+
+    @Test
+    void restarNegativos() {
+        assertEquals(0, calc.restar(-5, -5));
+
+    }
+
+
+    @Test
+    void restarZero() {
+        assertEquals(0, calc.restar(0, 0));
+    }
+
+
+    @Test
+    void restarZeroNegativos() {
+        assertEquals(0, calc.restar(-0, -0));
+    }
+
+
+    @Test
+    void multiplicarNormal() {
+        assertEquals(100, calc.multiplicar(10, 10));
+    }
+
+    @Test
+    void multiplicarNegativos() {
+        assertEquals(100, calc.multiplicar(-10,-10 ));
+    }
+
+    @Test
+    void multiplicarZero() {
+        assertEquals(0, calc.multiplicar(0, 1000000));
+    }
+}
+
+
