53b851dab89faf56ee172a2c5e4c69d4b98ad3b7
Rafael <augustico10@hotmail.com>
2025-08-17 14:28:48 -0500

    Calculadora básica
    aasd

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -1,12 +1,22 @@
 package calculadora;
 
 public class Calculadora {
+
     public int sumar(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public int dividir(int a, int b) {
-        if(b==0) throw new IllegalArgumentException("No se puede dividir por cero");
-        return a/b;
+        if (b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
+        return a / b;
+    }
+
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
     }
 }
