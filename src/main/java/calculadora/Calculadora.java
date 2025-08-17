1b645cd7b1b4a461dfecabf97baa4d849e195960
franky franco <frankyfranco456@gmail.com>
2025-08-17 15:53:10 -0500

    se agregan los test para suma y division

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -3,7 +3,12 @@ package calculadora;
 public class Calculadora {
 
     public int sumar(int a, int b) {
+        if (b == 0 ) throw new IllegalArgumentException("No se puede dividir por cero");
+        try{
             return a + b;
+        }catch(ArithmeticException ex){
+            throw new ArithmeticException("Argumentos no validos");
+        }
     }
     public int dividir(int a, int b) {
         if (b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
---
+++ src/test/java/CalculadoraTestFranky.java
@@ -0,0 +1,37 @@
+import calculadora.Calculadora;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+    public class CalculadoraTestFranky {
+    Calculadora calc = new Calculadora();
+
+    @Test
+    void testSuma(){
+        assertEquals(5, calc.sumar(2,3));
+    }
+
+    @Test
+    void testSumaNegativos(){
+        assertEquals(-7, calc.sumar(-4,-3));
+    }
+
+    @Test
+    void testSumaCero(){
+        assertEquals(3, calc.sumar(0,3));
+    }
+
+    @Test
+    void testDivision(){
+        assertEquals(2, calc.dividir(10, 5));
+    }
+
+    @Test
+    void testDivisionNegativos(){
+        assertEquals(-2, calc.dividir(-10, 5));
+    }
+
+    @Test
+    void testDividirPorCero(){
+        Exception ex =assertThrows(IllegalArgumentException.class, () -> calc.dividir(10,0));
+    }
+}
