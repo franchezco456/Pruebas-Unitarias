dfef341a378d9460f04ad93d8c0bd205962c0ae7
franky franco <frankyfranco456@gmail.com>
2025-08-17 14:49:49 -0500

    Correcciones a los metodos

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -3,21 +3,12 @@ package calculadora;
 public class Calculadora {
 
     public int sumar(int a, int b) {
-        try {
             return a + b;
-        }catch(ArithmeticException e) {
-            throw new ArithmeticException("Error al ejecutar la suma");
-        }
-
     }
 
-    public int dividir(int a, int b) {
+    public int dividir (int a, int b) {
         if (b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
-        try{
-            return a / b;
-        }catch(ArithmeticException e) {
-            throw new ArithmeticException("Error al ejecutar la division");
-        }
+        return a / b;
     }
 
     public int restar(int a, int b) {
